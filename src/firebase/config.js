680dec26b22b733990bd0408970004f75a2f3d38
sharath <admin@Macbook-Air.local>
2023-12-18 19:58:57 +0530

    firebase config added

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAQZ3-uGIFGHh2zUew18HLwIijdEm8XzfM",
+    authDomain: "old-clone-react.firebaseapp.com",
+    projectId: "old-clone-react",
+    storageBucket: "old-clone-react.appspot.com",
+    messagingSenderId: "752330575615",
+    appId: "1:752330575615:web:244c35a71ca1a81245c6ca",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
